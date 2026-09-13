6a5f007ef59cc8108153b77f8a5c5303d542e380
Johnnie Kearse III <jkearse3@gmail.com>
2026-09-13 13:30:06 -0400

    chore(neovim): remove unused Treesitter text objects
    Structural text object mappings are no longer part of the editing
    workflow.
    
    Remove the plugin setup, selection keymaps, and lock entry while
    retaining core Treesitter highlighting and parser management.

--- modules/home/neovim/nvim/lua/config/treesitter.lua
+++ modules/home/neovim/nvim/lua/config/treesitter.lua
@@ -1,7 +1,6 @@
 require("lib.config").run({
 	plugins = {
 		"https://github.com/nvim-treesitter/nvim-treesitter",
-		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	setup = function()
 		vim.api.nvim_create_autocmd("PackChanged", {
@@ -59,62 +58,5 @@ require("lib.config").run({
 				end
 			end,
 		})
-
-		-- Textobjects: select keymaps via nvim-treesitter-textobjects new API.
-		require("nvim-treesitter-textobjects").setup({
-			select = {
-				lookahead = true,
-			},
-		})
-
-		local select_textobject = function(query, desc)
-			return {
-				function()
-					require("nvim-treesitter-textobjects.select").select_textobject(
-						query,
-						"textobjects"
-					)
-				end,
-				desc,
-			}
-		end
-
-		local keymaps = {
-			["a="] = select_textobject("@assignment.outer", "Select outer part of an assignment"),
-			["i="] = select_textobject("@assignment.inner", "Select inner part of an assignment"),
-			["l="] = select_textobject("@assignment.lhs", "Select left hand side of an assignment"),
-			["r="] = select_textobject(
-				"@assignment.rhs",
-				"Select right hand side of an assignment"
-			),
-			["aa"] = select_textobject(
-				"@parameter.outer",
-				"Select outer part of a parameter/argument"
-			),
-			["ia"] = select_textobject(
-				"@parameter.inner",
-				"Select inner part of a parameter/argument"
-			),
-			["ai"] = select_textobject("@conditional.outer", "Select outer part of a conditional"),
-			["ii"] = select_textobject("@conditional.inner", "Select inner part of a conditional"),
-			["al"] = select_textobject("@loop.outer", "Select outer part of a loop"),
-			["il"] = select_textobject("@loop.inner", "Select inner part of a loop"),
-			["af"] = select_textobject("@call.outer", "Select outer part of a function call"),
-			["if"] = select_textobject("@call.inner", "Select inner part of a function call"),
-			["am"] = select_textobject(
-				"@function.outer",
-				"Select outer part of a method/function definition"
-			),
-			["im"] = select_textobject(
-				"@function.inner",
-				"Select inner part of a method/function definition"
-			),
-			["ac"] = select_textobject("@class.outer", "Select outer part of a class"),
-			["ic"] = select_textobject("@class.inner", "Select inner part of a class"),
-		}
-
-		for lhs, mapping in pairs(keymaps) do
-			vim.keymap.set({ "x", "o" }, lhs, mapping[1], { desc = mapping[2] })
-		end
 	end,
 })
