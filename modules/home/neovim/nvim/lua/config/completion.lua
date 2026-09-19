a3d9650ad6ee16287309f3471ececf8989e9a241
Johnnie Kearse III <jkearse3@gmail.com>
2026-09-18 22:28:49 -0400

    refactor(neovim): simplify completion configuration
    Use named helpers and guard clauses for popup-menu mappings and native
    LSP completion. Keep the Blink options separate from plugin loading and
    collapse repetitive single-setting tables.
    
    Declare mapping inputs, the LSP attachment event, Blink keymap commands,
    and Blink configuration APIs so LuaLS retains plugin-aware diagnostics.

--- modules/home/neovim/nvim/lua/config/completion.lua
+++ modules/home/neovim/nvim/lua/config/completion.lua
@@ -3,153 +3,103 @@ vim.opt.completeopt = "popup,fuzzy,menu,menuone,noinsert,noselect"
 vim.opt.shortmess:append("c") -- Suppress messages in the completion menu.
 
 -- Popup menu navigation keybindings.
-local function hijack_pum_key(target_key, input_key)
-	return function()
-		return vim.fn.pumvisible() == 1 and target_key or input_key
-	end
+---@param input_key string
+---@param pum_key string
+---@param description string
+local function set_pum_keymap(input_key, pum_key, description)
+	vim.keymap.set("i", input_key, function()
+		return vim.fn.pumvisible() == 1 and pum_key or input_key
+	end, { desc = description, expr = true, noremap = true })
 end
 
-vim.keymap.set(
-	"i",
-	"<C-h>",
-	hijack_pum_key("<C-e>", "<C-h>"),
-	{ desc = "PUM: Escape", expr = true, noremap = true }
-)
-vim.keymap.set(
-	"i",
-	"<C-j>",
-	hijack_pum_key("<Down>", "<C-j>"),
-	{ desc = "PUM: Down", expr = true, noremap = true }
-)
-vim.keymap.set(
-	"i",
-	"<C-k>",
-	hijack_pum_key("<Up>", "<C-k>"),
-	{ desc = "PUM: Up", expr = true, noremap = true }
-)
-vim.keymap.set(
-	"i",
-	"<C-l>",
-	hijack_pum_key("<Enter>", "<C-l>"),
-	{ desc = "PUM: Select", expr = true, noremap = true }
-)
+set_pum_keymap("<C-h>", "<C-e>", "PUM: Escape")
+set_pum_keymap("<C-j>", "<Down>", "PUM: Down")
+set_pum_keymap("<C-k>", "<Up>", "PUM: Up")
+set_pum_keymap("<C-l>", "<Enter>", "PUM: Select")
 
 -- Fallback LSP completion when no completion plugin is loaded.
+---@param event vim.api.keyset.create_autocmd.callback_args
+local function enable_native_lsp_completion(event)
+	local is_blink_cmp_loaded = pcall(require, "blink.cmp")
+	if is_blink_cmp_loaded then
+		return
+	end
+
+	local client = vim.lsp.get_client_by_id(event.data.client_id)
+	if not client or not client:supports_method("textDocument/completion") then
+		return
+	end
+
+	client.server_capabilities.completionProvider.triggerCharacters =
+		vim.split(".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", "")
+	vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(event)
-		local function is_completion_plugin_enabled()
-			local ok = pcall(require, "blink.cmp")
-			return ok
-		end
-
-		if not is_completion_plugin_enabled() then
-			local client = vim.lsp.get_client_by_id(event.data.client_id)
-			if client and client:supports_method("textDocument/completion") then
-				client.server_capabilities.completionProvider.triggerCharacters =
-					vim.split(".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", "")
-
-				vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-			end
-		end
-	end,
+	callback = enable_native_lsp_completion,
 })
 
 -- Blink.cmp completion plugin.
+---@param action blink.cmp.KeymapCommand
+---@return blink.cmp.KeymapCommand[]
+local function action_with_fallback(action)
+	return { action, "fallback" }
+end
+
+---@type blink.cmp.Config
+local blink_options = {
+	keymap = {
+		preset = "none",
+		["<c-h>"] = action_with_fallback("hide"),
+		["<c-j>"] = action_with_fallback("select_next"),
+		["<c-k>"] = action_with_fallback("select_prev"),
+		["<c-l>"] = action_with_fallback("accept"),
+		["<tab>"] = action_with_fallback("snippet_forward"),
+		["<s-tab>"] = action_with_fallback("snippet_backward"),
+	},
+	completion = {
+		menu = {
+			border = "rounded",
+			draw = { components = { label = { width = { max = 120 } } } },
+		},
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 0,
+			window = { border = "rounded" },
+		},
+		list = { selection = { auto_insert = false } },
+		accept = { auto_brackets = { enabled = false } },
+	},
+	signature = {
+		enabled = true,
+		window = {
+			border = "rounded",
+			show_documentation = true,
+		},
+	},
+	cmdline = {
+		enabled = true,
+		keymap = { preset = "inherit" },
+		completion = {
+			menu = { auto_show = true },
+			list = { selection = { auto_insert = false } },
+		},
+	},
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+}
+
 require("lib.config").run({
 	plugins = {
-		{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1.*") },
+		{
+			src = "https://github.com/saghen/blink.cmp",
+			version = vim.version.range("1.*"),
+		},
 	},
 	setup = function()
-		require("blink.cmp").setup({
-			keymap = {
-				preset = "none",
-				["<c-h>"] = {
-					"hide",
-					"fallback",
-				},
-				["<c-j>"] = {
-					"select_next",
-					"fallback",
-				},
-				["<c-k>"] = {
-					"select_prev",
-					"fallback",
-				},
-				["<c-l>"] = {
-					"accept",
-					"fallback",
-				},
-				["<tab>"] = {
-					"snippet_forward",
-					"fallback",
-				},
-				["<s-tab>"] = {
-					"snippet_backward",
-					"fallback",
-				},
-			},
-			completion = {
-				menu = {
-					border = "rounded",
-					draw = {
-						components = {
-							label = {
-								width = {
-									max = 120,
-								},
-							},
-						},
-					},
-				},
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 0,
-					window = {
-						border = "rounded",
-					},
-				},
-				list = {
-					selection = {
-						auto_insert = false,
-					},
-				},
-				accept = {
-					auto_brackets = {
-						enabled = false,
-					},
-				},
-			},
-			signature = {
-				enabled = true,
-				window = {
-					border = "rounded",
-					show_documentation = true,
-				},
-			},
-			cmdline = {
-				enabled = true,
-				keymap = {
-					preset = "inherit",
-				},
-				completion = {
-					menu = {
-						auto_show = true,
-					},
-					list = {
-						selection = {
-							auto_insert = false,
-						},
-					},
-				},
-			},
-			sources = {
-				default = {
-					"lsp",
-					"path",
-					"snippets",
-					"buffer",
-				},
-			},
-		})
+		---@type blink.cmp.API
+		local blink = require("blink.cmp")
+		blink.setup(blink_options)
 	end,
 })
